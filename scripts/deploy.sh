2dbd580dfbf05b2f4fa5932c145d2edf1224d052
Stu Zhao <z12y12l12@gmail.com>
2012-10-15 00:31:42 -0400

    Checking whether the install path exists.
    Also, using variable TARGET_PATH to specify install path.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+TARGET_PATH=~/.dotfiles
+
+# Check whether TARGET_PATH exists.
+if [ -d $TARGET_PATH ]; then
+    echo "It seems dotfiles are already installed. You have to remove $TARGET_PATH before continue installing."
+    exit 1
+fi
+
 # Check whether git exists.
 git --version &> /dev/null || {
      echo 'Git is not installed on this system.'
      exit 1
 }
 
-echo 'Cloning dotfiles to ~/.dotfiles'
-git clone https://github.com/zhaostu/dotfiles.git ~/.dotfiles
+echo "Cloning dotfiles to $TARGET_PATH"
+git clone https://github.com/zhaostu/dotfiles.git $TARGET_PATH
 
 echo 'Installing dotfiles.'
-~/.dotfiles/scripts/install.sh
+$TARGET_PATH/scripts/install.sh
 
 echo 'Finished!'
