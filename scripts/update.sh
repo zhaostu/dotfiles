7f6118f14fc88d515b4bf4116421c5fd01735230
Stu Zhao <z12y12l12@gmail.com>
2012-10-21 20:43:10 -0400

    Update dotfiles project itself when updating.

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,10 @@ export TOOLS_PATH=$(dirname $(readlink -f $0))
 . $TOOLS_PATH/config.sh
 . $TOOLS_PATH/functions.sh
 
+# Update the project itself.
+cd $BASE_PATH
+git pull origin master
+
 # Update all submodules
 cd $BASE_PATH
 git submodule foreach git pull origin master
