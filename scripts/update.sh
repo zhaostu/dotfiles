d6fea58956542cc628a3b54c08afd87f890d6cf8
Stu Zhao <z12y12l12@gmail.com>
2013-03-08 15:59:58 -0500

    Prevent update script from failing.
    `git stash pop` may return 1 when there is no valid stash.

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,9 +12,9 @@ $SCRIPTS_PATH/uninstall.sh
 
 # Update the project itself.
 cd $SCRIPTS_PATH
-git stash
+retval=$(git stash save -q)
 git pull origin master
-git stash pop
+retval=$(git stash pop -q)
 
 # Install the new version.
 $SCRIPTS_PATH/install.sh
