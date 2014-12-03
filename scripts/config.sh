1a0f0ab761474105bb69cf562a808626014fbee9
Stu Zhao <z12y12l12@gmail.com>
2014-12-02 22:16:18 -0500

    Added my own custom theme and use it.

--- scripts/config.sh
+++ scripts/config.sh
@@ -17,5 +17,5 @@ DOTFILES='gitconfig vim vimrc oh-my-zsh zshrc'
 
 # List of files to install to custom position, seperated by newline.
 # On each line, put the file and the target filename, seperated by space.
-CUSTOM_FILES='zsh-syntax-highlighting .oh-my-zsh/custom/plugins/zsh-syntax-highlighting'
-
+CUSTOM_FILES='zsh-syntax-highlighting .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+stu.zsh-theme .oh-my-zsh/custom/themes/stu.zsh-theme'
