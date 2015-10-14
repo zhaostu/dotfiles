36ff1e6b0a650d7860597a76b44c24bbba3d9909
Stu Zhao <stu.zhao@transloc.com>
2015-10-14 13:20:26 -0400

    Remove automatic git stash, make sure folder clean
    Instead of doing an automatic git stash before doing a git pull from
    master, we make sure the folder is clean by checking git status.
    Otherwise it's easy to forget to pop the stash back after upgrade.

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,16 +7,20 @@ set -u
 # The path containing the scripts.
 export SCRIPTS_PATH=$(dirname $(readlink -f $0))
 
+# Make sure the installation folder is clean
+GIT_STATUS_FLAG='--porcelain --ignore-submodules --untracked-files=no'
+GIT_DIRTY=$(cd $SCRIPTS_PATH; git status $GIT_STATUS_FLAG 2> /dev/null | tail -n1)
+
+if [ -n $GIT_DIRTY ]; then
+    echo "Your installation folder has uncommitted changes. Aborting."
+fi
+
 # Uninstall the old version.
 $SCRIPTS_PATH/uninstall.sh
 
 # Update the project itself.
 cd $SCRIPTS_PATH
 
-# The reason to config user email / name is so that `git stash` would work.
-git config user.email "z12y12l12@gmail.com"
-git config user.name "Stu Zhao"
-git stash save -q && :
 git pull origin master
 
 # Install the new version.
