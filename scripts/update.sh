9696e0672eefb1e7038ce9bca19585fefe613497
Stu Zhao <z12y12l12@gmail.com>
2016-10-25 11:18:46 -0400

    Make sure name and email is set when updating

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,12 +10,14 @@ GIT_DIRTY=$(cd $SCRIPTS_PATH; git status $GIT_STATUS_FLAG 2> /dev/null | tail -n
 # Uninstall the old version.
 $SCRIPTS_PATH/uninstall.sh
 
-# Update the project itself.
 cd $SCRIPTS_PATH
 
-git stash
-git pull origin master
-git stash pop
+# Make sure the name and email is set
+git config user.name "Stu Zhao"
+git config user.email "z12y12l12@gmail.com"
+
+# Update the project itself.
+git pull --rebase --autostash origin master
 
 # Install the new version.
 $SCRIPTS_PATH/install.sh
