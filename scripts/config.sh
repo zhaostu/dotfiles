67de730b701bb69a34869a9bb1c91b3950822aa2
Stu Zhao <z12y12l12@gmail.com>
2020-05-08 21:56:56 -0700

    Remove language based zsh plugins

--- scripts/config.sh
+++ scripts/config.sh
@@ -18,7 +18,6 @@ DOTFILES='gitconfig gitignore_global vimrc oh-my-zsh zshrc profile i3 Xresources
 # List of files to install to custom position, seperated by newline.
 # On each line, put the file and the target filename, seperated by space.
 CUSTOM_FILES='zsh-syntax-highlighting .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-virtualenvwrapper .oh-my-zsh/custom/plugins/virtualenvwrapper
 stu.zsh-theme .oh-my-zsh/custom/themes/stu.zsh-theme
 workrave.ini .workrave/workrave.ini
 termite .config/termite
